31eefd71a959deed9265f6f96bf3456de1a5cb87
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-11-07 18:58:50 -0200

    add test error handler

--- src/parser.js
+++ src/parser.js
@@ -1,17 +1,27 @@
 const rxPage = /[?|&]page=(\d+)/
 const rxRel = /rel="(.+)"/
 
-const parser = str => str.split(',')
-  .map(section => section.split(';'))
-  .map(section => {
-    const page = Number(rxPage.exec(section[0])[1])
-    const rel = rxRel.exec(section[1])[1]
-    return [ page, rel ]
-  })
-  .reduce((acc, value, index) => {
-    acc[value[1]] = value[0]
+const parser = str => {
+  if (typeof str !== 'string') {
+    throw new Error('str must be a string')
+  }
 
-    return acc
-  }, {})
+  if (str.length === 0) {
+    throw new Error('str is a invalid string')
+  }
+
+  return str.split(',')
+    .map(section => section.split(';'))
+    .map(section => {
+      const page = Number(rxPage.exec(section[0])[1])
+      const rel = rxRel.exec(section[1])[1]
+      return [ page, rel ]
+    })
+    .reduce((acc, value, index) => {
+      acc[value[1]] = value[0]
+
+      return acc
+    }, {})
+}
 
 module.exports = parser
--- src/parser.test.js
+++ src/parser.test.js
@@ -4,3 +4,9 @@ const mock = require('../mock')
 test('mockfile parsed from parser function', () => {
   expect(parser(mock)).toEqual({ next: 4, last: 100, first: 1, prev: 2 })
 })
+
+test('handler error', () => {
+  expect(() => { parser() }).toThrowError('str must be a string')
+
+  expect(() => { parser('') }).toThrowError('str is a invalid string')
+})
