01ea020166e7d5ecddba485c22cbcbc9103e7479
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-11-16 17:04:00 -0200

    indent

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const assertStr = require('./libs/assert-str')
 const compose = require('./support/compose')
-const { mountObject, regexFields, splitFields, splitInSections } = require('./libs')
+const {
+  mountObject,
+  regexFields,
+  splitFields,
+  splitInSections
+} = require('./libs')
 
 const parser = compose(mountObject, regexFields, splitFields, splitInSections)
 
